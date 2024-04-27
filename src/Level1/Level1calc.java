acf86ffad25e4d846e5c719ab7d7e78cc85cce3d
Kang Hyun Ji / Qwen <102335813+onda0-0@users.noreply.github.com>
2024-04-27 20:35:27 +0900

    1~4 요구조건 구현 완료, 5번 구현중

--- src/Level1/Level1calc.java
+++ src/Level1/Level1calc.java
@@ -4,44 +4,65 @@ import java.util.Scanner;
 
 public class Level1calc {
     public static void main(String[] args) {
+
+        /*
+        * 1. Scanner를 사용하여 양의 정수 2개(0 포함)를 전달 받을 수 있습니다.
+        * 2. Scanner를 사용하여 사칙연산 기호를 전달 받을 수 있습니다. (charAt(0))
+        * 3. 입력받은 양의 정수 2개와 사칙연산 기호를 사용하여 연산을 진행한 후 결과값을 출력합니다.
+        *       3-1) 사칙연산 기호에 맞는 연산자를 사용하여 연산을 진행합니다.
+        *       3-2) 입력받은 연산 기호를 구분하기 위해 제어문을 사용합니다. (e.g.if, switch)
+        *       3-3) 연산 오류가 발생할 경우 해당 오류에 대한 내용을 정제하여 출력합니다.
+        *           - e.g. “나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다. “
+        * 4. 반복문을 사용하여 반복의 종료를 알려주는 “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행할 수 있도록 소스 코드를 수정합니다.
+        *       - 반복문을 사용합니다. (e.g. for, while …)
+        * 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
+        *       5-1) 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
+        *       5-2) 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+        * */
+
         Scanner sc = new Scanner(System.in);
         String continueOrNot="";
+        // 5-1)연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성 & 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성
+        int[] results= new int[10];
+        // 5-2)연산의 결과가 저장된 배열의 마지막 index를 저장하는 변수를 선언
+        int index=0;
 
+        // 4) “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행
         while (!(continueOrNot.equals("exit"))){
             System.out.print("첫 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num1=sc.nextInt();
+            int num1=sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장
 
             System.out.print("두 번째 숫자를 입력하세요: ");
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            int num2=sc.nextInt();
+            int num2=sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장
 
             System.out.print("사칙연산 기호를 입력하세요: ");
-            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.  charAt(0)
-            String operator=sc.next();
-            //char charAt(0)
+            char operator=(sc.next()).charAt(0); // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장. charAt(0)
+
             int result = 0;
+
             switch (operator){
-                case "+":  result=num1+num2;
+                case '+':  result=num1+num2;
                     break;
-                case "-":  result=num1-num2;
+                case '-':  result=num1-num2;
                     break;
-                case "*":  result=num1*num2;
+                case '*':  result=num1*num2;
                     break;
-                case "/":  if(num2==0){
+                case '/':  if(num2==0){
                     System.out.print("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
                 }else{result=num1/num2;
                     break;}
             }
-            /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+
+            //index=index%10; 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현하려면 이렇게 하면 안됨
+            results[index]=result;
+            index++;
+
             System.out.println("결과: " + result);
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            /* exit을 입력 받으면 반복 종료 */
-            continueOrNot=sc.next();
+
+            continueOrNot=sc.next(); // exit을 입력 받으면 반복 종료되게 하기 위해 변수에 넣음
         }
-
-
 
     }
 }
