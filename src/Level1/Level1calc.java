2add2294de170d22f13f20686b45e0382b2c40b1
Kang Hyun Ji / Qwen <102335813+onda0-0@users.noreply.github.com>
2024-04-29 15:19:46 +0900

    1-6완료. 특히 6에서 조건문 부분 수정/7번요구조건도 해결
    6에서 문제가 인덱스가9를 초과해도 안되고, 0-9를 다 채우고 넘칠때 한칸씩 당겨져서 저장되는것이 문제였는데, i++를 else에만 넣었더니 if부분일때는 (즉, 10번째부터는)++의 과정이 없어져서 그이후로 인덱스 갱신이 안되는 문제가 있었고, 그래서 if부분에 --와 ++을 구현했다.

--- src/Level1/Level1calc.java
+++ src/Level1/Level1calc.java
@@ -1,5 +1,6 @@
 package Level1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Level1calc {
@@ -18,6 +19,14 @@ public class Level1calc {
         * 5. 연산 결과 10개를 저장할 수 있는 배열을 선언 및 생성하고 연산의 결과를 저장합니다.
         *       5-1) 연산의 결과를 저장할 수 있도록 적합한 타입의 배열을 생성합니다.
         *       5-2) 연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 합니다.
+        * 6. 연산 결과가 10개를 초과하는 경우 가장 먼저 저장된 결과를 삭제하고 새로운 연산 결과가 저장될 수 있도록 소스 코드를 수정합니다.
+        *       - 현재 저장된 index가 마지막(9)라면 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현합니다.
+        *       - (결과 값들이 한칸씩 앞으로 이동되게 how??)
+        *
+        *
+        *
+        *
+        *
         * */
 
         Scanner sc = new Scanner(System.in);
@@ -47,18 +56,69 @@ public class Level1calc {
                     break;
                 case '*':  result=num1*num2;
                     break;
-                case '/':  if(num2==0){
-                    System.out.print("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    break;
-                }else{result=num1/num2;
-                    break;}
+                case '/':
+                    if(num2==0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다."); //입력안된다고 나오고 다시 돌아갈수 있게 해야함.
+                        continue;
+                    }else{
+                        result=num1/num2;
+                        break;}
             }
 
             //index=index%10; 가장 먼저 저장된 결과 값이 삭제 되고 새로운 결과 값이 마지막 index에 저장될 수 있도록 구현하려면 이렇게 하면 안됨
-            results[index]=result;
-            index++;
+
+            /*이 방식의 문제를 발견했다.
+            * 이렇게 하고 배열에 어떻게 담기는지 출력해봤더니
+            * results[8]와 results[9]에 이중저장된다.
+            * if(index==9){
+                results[index]=result;
+                for(int i = 0; i<results.length-1; i++){
+                    results[i]=results[i+1];//
+                }
+            }
+            * if(index==9){
+                for(int i = 0; i<results.length-1; i++){
+                    results[i]=results[i+1];
+                }results[index]=result;
+            }//results[8]에 저장이 안되고 results[9]로 넘어가버린다.
+            *
+            *
+            *  if(index>9){
+                index--;
+                for(int i = 0; i<results.length-1; i++){
+                    results[i]=results[i+1];//
+                }results[index]=result;
+
+            }else{
+                results[index]=result;
+                index++; //이렇게 했더니 한번 돌고나면 갱신이 안됨-> 왜냐면 index++의 과정을 이제 안겪거든.
+            }
+            *
+            *  if(index>9){
+                for(int i = 0; i<results.length-1; i++){
+                    results[i]=results[i+1];//
+                }results[index]=result;
+
+            }else{
+                results[index]=result;
+                index++; //연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count 하면 index10초과되어 오류발생
+            * */
+            if(index>9){
+                for(int i = 0; i<results.length-1; i++){
+                    results[i]=results[i+1];//
+                }
+                index--;
+                results[index]=result;
+                index++;
+
+            }else{
+                results[index]=result;
+                index++; //연산의 결과를 비어있는 곳에 저장하기 위해 저장할 때마다 count
+            }
+
 
             System.out.println("결과: " + result);
+            System.out.println("배열에담긴것결과: " + Arrays.toString(results));
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
 
             continueOrNot=sc.next(); // exit을 입력 받으면 반복 종료되게 하기 위해 변수에 넣음
---
+++ src/Level1/Level1calc_Collection.java
@@ -0,0 +1,80 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Level1calc_Collection {
+    public static void main(String[] args) {
+
+        /*
+         * 1~6번 동일
+         * 7. 연산 결과가 10개로 고정되지 않고 무한이 저장될 수 있도록 소스 코드를 수정합니다.
+         *       - JCF(Java Collection Framework)를 사용합니다. (e.g. List, Set …)
+         *       - “remove”라는 문자열을 입력받으면 가장 먼저 저장된 결과가 삭제될 수 있도록 구현합니다.
+         * 8. “inquiry”라는 문자열이 입력되면 저장된 연산 결과 전부를 출력합니다.
+         *       - foreach(향상된 for문)을 활용하여 구현 해봅니다.
+         *
+         *
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        String continueOrNot="";
+
+        List<Integer> results= new ArrayList<Integer>();
+        int index=0;
+
+        // 4) “exit” 문자열을 입력하기 전까지 무한으로 계산을 진행
+        while (!(continueOrNot.equals("exit"))){
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1=sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장
+
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2=sc.nextInt(); // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator=(sc.next()).charAt(0); // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장. charAt(0)
+
+            int result = 0;
+
+            switch (operator){
+                case '+':  result=num1+num2;
+                    break;
+                case '-':  result=num1-num2;
+                    break;
+                case '*':  result=num1*num2;
+                    break;
+                case '/':
+                    if(num2==0){
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다."); //입력안된다고 나오고 다시 돌아갈수 있게 해야함.
+                        continue;
+                    }else{
+                        result=num1/num2;
+                        break;}
+            }
+
+            results.add(result);
+
+            System.out.println("결과: " + result);
+            System.out.println("배열(리스트)결과: " + Arrays.toString(results.toArray(new Integer[results.size()])));
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String wannaRemove=sc.next();
+            System.out.println("배열(리스트)결과: " + Arrays.toString(results.toArray(new Integer[results.size()])));
+
+            if(wannaRemove.equals("remove")){
+                results.remove(0);//remove안해도 List에 0번째 객체가 들어가진 않겠지만 객체 자체는 여전히 남아 있으니까 지워야 할듯.
+               /*
+               results.remove(0);//remove안해도 List에 0번째 객체가 들어가진 않겠지만 객체 자체는 여전히 남아 있으니까 지워야 할듯.
+               *  for(int i = 0; i<results.size()-1; i++){
+                    results.set(i, results.get(i + 1));//
+                } //이렇게 구현하고 확인해보니 지워지는게 이상했다. 그래서 인덱스를 당기는 과정없이 그냥 remove하니까 잘된다. remove를 쓰면자동으로 자리이동이된다.
+               * */
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            continueOrNot=sc.next(); // exit을 입력 받으면 반복 종료되게 하기 위해 변수에 넣음
+        }
+
+    }
+}
