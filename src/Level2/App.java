3dc4d8b1a168aa60a4bcf9437c7edede9db79340
Kang Hyun Ji / Qwen <102335813+onda0-0@users.noreply.github.com>
2024-05-01 12:56:15 +0900

    요구조건 일부 구현. 선언만하고 실제로 구현하지 않은것도 있음

--- src/Level2/App.java
+++ src/Level2/App.java
@@ -1,26 +1,49 @@
 package Level2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+
         /* Calculator 인스턴스 생성 */
         Calculator calc=new Calculator();
         Scanner sc = new Scanner(System.in);
+        String continueOrNot="";
 
-        /* 반복문 시작 */
-        System.out.print("첫 번째 숫자를 입력하세요:");
-        int num1 = sc.nextInt();
-        System.out.print("두 번째 숫자를 입력하세요:");
-        int num2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        char operator = sc.next().charAt(0);
+        while (!(continueOrNot.equals("exit"))) {
+            /* 반복문 시작 */
+            System.out.print("첫 번째 숫자를 입력하세요:");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요:");
+            int num2 = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.next().charAt(0);
 
-        calc.calculate(num1,num2,operator);
-        /* 위 요구사항에 맞게 소스 코드 수정 */
 
-        System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            ...
-        /* 반복문 종료 */
+            //Calculator 클래스가 활용될 수 있도록 수정
+            //직접접근 calc.results.add(calc.calculate(num1,num2,operator));
+            //3. 간접접근 -> private으로 필드 만들고 getter/setter함수를 통해 접근
+
+
+            //5.저장된 연산 결과들을 조회하는 기능을 가진 메서드 활용
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            String wannaInquiry=sc.next();
+
+            if(wannaInquiry.equals("inquiry")){
+                calc.inquiryResults();
+            }
+            //4.가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드 활용
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+            String wannaRemove=sc.next();
+
+            if(wannaRemove.equals("remove")){
+              calc.removeResult();
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            continueOrNot = sc.next(); // exit을 입력 받으면 반복 종료되게 하기 위해 변수에 넣음
+        }
     }
 }
--- src/Level2/Calculator.java
+++ src/Level2/Calculator.java
@@ -6,12 +6,46 @@ import java.util.Scanner;
 
 public class Calculator {
     /* 연산 결과를 저장하는 컬렉션 타입 필드 선언 및 생성 */
-    Scanner sc = new Scanner(System.in);
-    List<Integer> results= new ArrayList<Integer>();
+    private List<Double> results= new ArrayList<Double>();
+    //7.원의 넓이 결과를 저장하는 컬렉션 타입의 필드 선언 및 생성
+    private List<Double> areas= new ArrayList<Double>();
 
-    public Double calculate(int num1, int num2, char operator) {
+    //6. calculator 인스턴스를 생성(new)할 때 생성자를 통해 연산 결과를 저장하고 있는 컬렉션 필드가 초기화 되도록 수정
+    public Calculator() {
+        this.results = new ArrayList<Double>();
+    }
+
+    Scanner sc = new Scanner(System.in);
+    // 나눗셈 메서드, 분모가 0일 때 예외 발생
+    public double divide(int num1, int num2) throws BadInputException {
+        if (num2 == 0) {
+            throw new BadInputException("분모가 0이 될 수 없으니, 분모에 0을 제외한 수"); // 분모가 0일 때 예외 발생
+        }
+        return num1 / num2; // 정상적인 나눗셈
+    }
+    //1. 양의 정수 2개(0 포함)와 연산 기호를 매개변수로 받아 사칙연산(+,-,*,/) 기능을 수행한 후 결과 값을 반환하는 메서드
+    public Double calculate(int num1, int num2, char operator) throws BadInputException{
         double outcome=0;
-        /* 위 요구사항에 맞게 구현 */
+
+        switch (operator){
+            case '+':
+                outcome = num1 + num2;
+                return outcome;
+            case '-':
+                outcome = num1 - num2;
+                return  outcome;
+            case '*':
+                outcome = num1 * num2;
+                return outcome;
+            case '/':
+                return divide(num1,num2);
+            default:
+                throw new BadInputException("+,-,/,*중 하나");
+
+        }
+
+        /*double outcome=0;
+
         if(operator=='+'){
             outcome = num1 + num2;
         }else if(operator=='-'){
@@ -21,9 +55,56 @@ public class Calculator {
         }else if(operator=='/'){
             outcome = num1 / num2;
         }
-        return outcome;
+        return outcome;*/
     }
 
-    //results.add(outcome);
+    //5.저장된 연산 결과들을 조회하는 기능을 가진 메서드
+    public void inquiryResults() {
+        for (double resultsItems : results) {
+            System.out.print(resultsItems);
+        }
+    }
 
+    //4.가장 먼저 저장된 데이터를 삭제하는 기능을 가진 메서드
+    public void removeResult() {
+        results.remove(0);
+    }
+
+    //3. getter
+    public List<Double> getResults(){
+            return results;
+    }
+
+    //3. setter
+    public void setResults(List<Double> results){
+        this.results=results;
+    }
+
+    public void addResults(Double result){
+        this.results.add(result);
+    }
+
+
+    //7.반지름을 매개변수로 전달받아 원의 넓이를 계산하여 반환해주는 메서드를 구현
+    public double calculateCircleArea(int radius) {
+        /* 원의 넓이 계산 구현 */
+        return (double)radius*(double)radius*3.14;
+    }
+
+    //7. 원의 넓이 저장 필드 Getter
+    public List<Double> getAreas(){
+        return areas;
+    }
+
+    //7.원의 넓이 저장 필드 Setter
+    public void setAreas(List<Double> areas){
+        this.areas=areas;
+    }
+
+    //5.저장된 연산 결과들을 조회하는 기능을 가진 메서드
+    public void inquiryAreas() {
+        for (double areasItems : areas) {
+            System.out.print(areasItems);
+        }
+    }
 }
